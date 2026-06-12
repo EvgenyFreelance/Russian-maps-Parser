a5027854380f07b6d17b8824959409f9f1eb42c9
JackPodrochitel-Cdur <emashevruslan13@gmail.com>
2026-06-13 01:18:47 +0700

    Добавление экспорта в sqlite и excel таблицу

--- export_data.py
+++ export_data.py
@@ -1,35 +1,44 @@
 from  main import Parser
 import sqlite3
+import pandas as pd
 
-connection = sqlite3.connect('outputs/parsing_results.db')
-cursor = connection.cursor()
+def to_sqlite(data,db_name='parsing_results'):
+    connection = sqlite3.connect(f'outputs/{db_name}.db')
+    cursor = connection.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS Parse_results (
-Id INTEGER PRIMARY KEY,
-Organization TEXT NOT NULL,
-Address TEXT NOT NULL,
-Phone_number TEXT NOT NULL,
-URL_on_maps TEXT NOT NULL,
-URL_site TEXT NOT NULL,
-Keywords TEXT NOT NULL
-)
-""")
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS Parse_results (
+    Id INTEGER PRIMARY KEY,
+    Organization TEXT NOT NULL,
+    Address TEXT NOT NULL,
+    Phone_number TEXT NOT NULL,
+    URL_on_maps TEXT NOT NULL,
+    URL_site TEXT NOT NULL,
+    Keywords TEXT NOT NULL
+    )
+    """)
 
-query = 'INSERT OR REPLACE INTO Parse_results (Id, Organization, Address, Phone_number, URL_on_maps, URL_site, Keywords) VALUES (?, ?, ?, ?, ?, ?, ?)'
+    query = 'INSERT OR REPLACE INTO Parse_results (Id, Organization, Address, Phone_number, URL_on_maps, URL_site, Keywords) VALUES (?, ?, ?, ?, ?, ?, ?)'
 
-parser = Parser(url='https://yandex.ru/maps',place='Ярославль',institution='Больницы')
-parser.parse()
+    cursor.executemany(query,data)
 
-result = parser.get_result()
-result = [
-    x for x in zip( range(1,len(result['Название'])+1) , *result.values() )
-]
+    connection.commit()
+    connection.close()
 
-cursor.executemany(query,result)
 
-connection.commit()
-connection.close()
+def to_excel(data,path='outputs/parsing_results.xlsx'):
+    df = pd.DataFrame(data)
+
+    with pd.ExcelWriter(path, engine='openpyxl', mode='w') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
 
 if __name__ == '__main__':
-    pass
+    parser = Parser(url='https://yandex.ru/maps', place='Ярославль', institution='Больницы')
+    parser.parse()
+
+    result = parser.get_result()
+    to_excel(result)
+    result = [x for x in zip( range(1, len(result['Название']) + 1), *result.values() )]
+    to_sqlite(result)
+
+
